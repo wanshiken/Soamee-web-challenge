93f9b58941a0043028c96dca527757c8d6dea889
Juan De Santis <juanbds66@gmail.com>
2021-11-08 13:56:09 +0100

    deploy

--- server/app.js
+++ server/app.js
@@ -20,9 +20,9 @@ const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
 
 app.use((req, res) => res.sendFile(__dirname + "/public/index.html"));
+require("./error-handling")(app);
 
 
 
